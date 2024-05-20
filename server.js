01e4c635b7405f5b509f5c6defa920340bbc4cef
Jaydeep Pathak <jaydeep3005@gmail.com>
2024-05-20 16:21:49 +0530

    responseFormatter added

--- server.js
+++ server.js
@@ -2,13 +2,17 @@ const express = require("express");
 const dotenv = require("dotenv").config();
 const noteRoutes = require("./routes/noteRoutes");
 const connectDb = require("./config/dbConnection");
+// const errorHandler = require("./middleware/errorHandler");
+const responseFormatter = require("./middleware/responseFormatter");
 
 connectDb();
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(express.json());
+app.use(responseFormatter);
 app.use("/api/notes", noteRoutes);
+// app.use(errorHandler);
 
 app.listen(port, () => {
     console.log(`Listeing on port ${port}`);
