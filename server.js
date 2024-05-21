5668c338c731dd9a221612f3661478555fee50c9
Jaydeep Pathak <jaydeep3005@gmail.com>
2024-05-21 09:59:14 +0530

    ..

--- server.js
+++ server.js
@@ -5,12 +5,10 @@ const userRoutes = require("./routes/userRoutes");
 const connectDb = require("./config/dbConnection");
 // const errorHandler = require("./middleware/errorHandler");
 const responseFormatter = require("./middleware/responseFormatter");
-const serverless = require("serverless-http");
 
 connectDb();
 const app = express();
 const port = process.env.PORT || 5000;
-const router = express.Router();
 
 app.use(express.json());
 app.use(responseFormatter);
@@ -18,9 +16,6 @@ app.use("/api/notes", noteRoutes);
 app.use("/api/users", userRoutes);
 // app.use(errorHandler);
 
-// app.listen(port, () => {
-//     console.log(`Listeing on port ${port}`);
-// });
-
-app.use("/.netlify/functions/api", router);
-export const handler = serverless(app);
+app.listen(port, () => {
+    console.log(`Listeing on port ${port}`);
+});
