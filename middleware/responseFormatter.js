c0f2af84127a36699318edb9c6c68c84bc586e43
Jaydeep Pathak <jaydeep3005@gmail.com>
2024-05-23 00:40:29 +0530

    winston added for loggin

--- middleware/responseFormatter.js
+++ middleware/responseFormatter.js
@@ -1,5 +1,13 @@
+const winston = require("winston");
+
+const logger = winston.createLogger({
+    level: "error",
+    format: winston.format.json(),
+    transports: [new winston.transports.Console()],
+});
 const responseFormatter = (req, res, next) => {
     res.sendSuccess = (data, message = "Success") => {
+        logger.info(message);
         res.status(200).json({
             success: true,
             data,
@@ -8,6 +16,7 @@ const responseFormatter = (req, res, next) => {
     };
 
     res.sendError = (error, statusCode = 500) => {
+        logger.error(message);
         res.status(statusCode).json({
             success: false,
             message: error.message || "Something went wrong",
