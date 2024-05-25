d197b78d3859cbb58ad863e0500a8362ab6db91b
Jaydeep Pathak <jaydeep3005@gmail.com>
2024-05-25 22:14:02 +0530

    minor

--- server.js
+++ server.js
@@ -11,8 +11,8 @@ connectDb();
 const app = express();
 const port = process.env.PORT || 5000;
 
+app.use(cors());
 app.use(express.json());
-app.use(cors({ origin: "*" }));
 app.use(responseFormatter);
 
 app.use("/api/notes", noteRoutes);
