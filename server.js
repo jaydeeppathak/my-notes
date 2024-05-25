d4f66c881b199b7a90599f8d6faf40fe3fd76370
Jaydeep Pathak <jaydeep3005@gmail.com>
2024-05-25 22:06:52 +0530

    added cors handling

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const dotenv = require("dotenv").config();
+const cors = require("cors");
 const noteRoutes = require("./routes/noteRoutes");
 const userRoutes = require("./routes/userRoutes");
 const connectDb = require("./config/dbConnection");
@@ -11,7 +12,9 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(express.json());
+app.use(cors({ origin: "*" }));
 app.use(responseFormatter);
+
 app.use("/api/notes", noteRoutes);
 app.use("/api/users", userRoutes);
 // app.use(errorHandler);
